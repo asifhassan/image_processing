df8dc583ebde494d0a5d96a4777f8f08440926c3
Asif Hassan <asif08ece@gmail.com>
2018-12-18 15:00:20 -0800

    edited after 1

--- rgb_gray_convert.py
+++ rgb_gray_convert.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mp
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])# multiplication using R,G,B value 
 
-img = mp.imread('pic_10.jpg')     
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])# multiplication using R,G,B value
+
+
+
+img = mp.imread('VPICTURE_ASIF HASSAN.jpg')     
 gray = rgb2gray(img)
 cmap=plt.get_cmap('gray')
 plt.imshow(gray,cmap)
